a7875531b071a9099c5afb776013171dc25ac6d3
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-04 15:08:48 -0300

    Criando um CRUD básico para consumo no Postman

--- NoFood.Api/server.js
+++ NoFood.Api/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.status(200).send('Olá Mundo!');
+// CRUD - Verbs http (verbos http)
+// Retorna sempre um status code http:
+// https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+// C - CREATE
+app.post('/', (req, res) =>{
+    res.status(201).send('Registro Criado!');
 });
+
+//R - READ
+app.get('/', (req, res) =>{
+    res.status(200).send('Olá Mundo Alterado!');
+});
+
+//U - UPDATE
+app.put('/', (req, res) =>{
+    res.status(202).send('Registro Atualizado!');
+});
+
+//D - DELETE
+app.delete('/', (req, res) =>{
+    res.status(204).send('Registro Excluído');
+});
+
+//400 - bad request (utilizado para validações)
+//401 - Unauthorized
+//500 - Internal Server Error (erros de servidor)
 
 app.listen(3000, () =>{
     console.log('Servidor Api NoFood iniciado na porta 3000. Acesse http://localhost:3000');
