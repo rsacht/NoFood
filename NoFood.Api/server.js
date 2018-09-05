325d977797164d4bd11c0eab903f117441f6e4ba
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-04 23:41:28 -0300

    Aplicando a configuração da Porta

--- NoFood.Api/server.js
+++ NoFood.Api/server.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const app = require('../NoFood.Api/bin/express');
+const variables = require('../NoFood.Api/bin/configuration/variables');
 
 app.listen(3000, () =>{
     console.info('Api inicializada com sucesso na porta 3000. Acesse http://localhost:3000');
