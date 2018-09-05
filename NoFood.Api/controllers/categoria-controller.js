bf014b3ae9373cef88a49efa7de548fa7c5a19fb
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-05 00:25:00 -0300

    Iniciando Controllers da rota Categoria

---
+++ NoFood.Api/controllers/categoria-controller.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function categoriaController(){
+
+}
+
+categoriaController.prototype.post = async (req, res) =>{};
+
+categoriaController.prototype.post = async (req, res) =>{};
+categoriaController.prototype.put = async (req, res) =>{};
+categoriaController.prototype.get = async (req, res) =>{};
+categoriaController.prototype.getById = async (req, res) =>{};
+categoriaController.prototype.delete = async (req, res) =>{};
