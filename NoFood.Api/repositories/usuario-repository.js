727796edbfdb4e5fec0fd52832a6c73474dc4a4a
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-06 17:16:19 -0300

    Refatorando o Projection

--- NoFood.Api/repositories/usuario-repository.js
+++ NoFood.Api/repositories/usuario-repository.js
@@ -6,15 +6,17 @@ class usuarioRepository {
 
     constructor() {
         this._base = new base('Usuario');
+        this._projection = 'nome email _id';
     }
     
     async authenticated(Email, Senha){
         let _hashSenha = md5(Senha);
-        this._base._model.findOne({email: Email, senha: _hashSenha}, 'nome email _id');
+        this._base._model.findOne({email: Email, senha: _hashSenha}, this._projection );
     }
 
     async create(data) {
-        return await this._base.create(data);
+        let usuarioCriado = await this._base.create(data);
+        return this._base._model.findById(usuarioCriado._id, this._projection );
     }
 
     async update(id, data) {
