c7d3a0e7db980b7400c96091d057cc9387329837
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-06 16:24:48 -0300

    Arquivo de Validações

--- NoFood.Api/bin/base/controller-base.js
+++ NoFood.Api/bin/base/controller-base.js
@@ -4,7 +4,7 @@ exports.post = async(repository, validationContract, req, res)=>{
         if(!validationContract.isValid()){
             res.status(400).send({
                 message: 'Existem dados inválidos na sua requisição',
-                validation: validationContract.error()
+                validation: validationContract.errors()
             }).end();
             return;
         }
@@ -23,7 +23,7 @@ exports.put = async(repository, validationContract, req, res)=>{
         if(!validationContract.isValid()){
             res.status(400).send({
                 message: 'Existem dados inválidos na sua requisição',
-                validation: validationContract.error()
+                validation: validationContract.errors()
             }).end();
             return;
         }
